fa41b6a332bd5b6d58d3628e270cb41820c12003
Orel Hazan <orelhaz@post.bgu.ac.il>
2017-01-23 14:27:15 +0200

    yalla nigmar

--- hotelManagement.py
+++ hotelManagement.py
@@ -1,1 +1,13 @@
-print("vfda")
+import hotelWorker
+
+
+def isThereDB():
+    pass
+
+
+def areAnyTaskLeft():
+    pass
+
+
+while(isThereDB() and areAnyTaskLeft()) :
+    hotelWorker.dohoteltask("", "")
---
+++ hotelWorker.py
@@ -0,0 +1,4 @@
+
+def dohoteltask(taskname, parameter):
+    pass
+
